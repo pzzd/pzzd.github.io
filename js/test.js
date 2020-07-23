35a031384c3ec4dac79d3cf9cd843cb91bac86a1
PZZD <pzzd@users.noreply.github.com>
2020-07-23 15:57:04 -0500

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,2 +1,17 @@
+console.log('20200723 3:56 pm');
+
 fetch('https://www.sans.org/tip-of-the-day/rss')
 			.then(response => console.log(response.text()));
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     console.log(this.responseText);
+    }
+  };
+  xhttp.open("GET", 'https://www.sans.org/tip-of-the-day/rss', true);
+  xhttp.send();
+}
+
+loadDoc();
